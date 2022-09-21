cda81d6d6375f1dedb579807c15680fd02997492
Vyoma Mohan <vyoma98@gmail.com>
2022-09-22 03:21:46 +0530

    Few edits and new file

--- practicefile1.py
+++ practicefile1.py
@@ -50,3 +50,18 @@ list_6 = ["one", "two"]
 list_7 = list(list_6)
 list_6[1] = "three"
 print(list_7)
+
+var_to_round = 3.141592653589793238
+print(round(var_to_round,4))
+
+list_8 = [2, 4, 7, 1, 3, 5, 6]
+print(sorted(list_8, reverse = True))
+
+print(list_8.index(7))
+print(list_8.count(7))
+
+test_string_1 = "abel tresfaye"
+print(test_string_1.capitalize())
+print(test_string_1.replace("tresfaye", "weeknd"))
+
+#Use append to add to lists
---
+++ practicefile2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 02:45:18 2022
+
+@author: vyoma
+"""
+
+import numpy as np
+
+array_1 = np.array([1,2,3])
+print(array_1)

941bbeef9671b3acb59cb9663ae53bc4d1286efc
Vyoma Mohan <vyoma98@gmail.com>
2022-09-22 02:04:32 +0530

    Create practicefile1.py

---
+++ practicefile1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Hello world")
+
+var1 = "testing"
+print(type(var1))
+
+pi_string = "3.14"
+pi_float= float(pi_string)
+print(type(pi_float))
+
+#List can contain diff types
+list_1 = ["cat", 3.14, True]
+print(list_1)
